3a73007c962d4a2b39d55a0c1490d5c5ee3a2b4d
Surcury-2 <ijorambmak@gmail.com>
2025-12-17 01:09:52 +0000

    changes

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
 window.addEventListener('scroll', function() {
     const footer = document.querySelector('.footer');
 
     // Check if the user has scrolled to the very bottom of the page
-    if (hasVerticalScrollbar){ 
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-            footer.classList.add('show'); // Add 'show' class to reveal the footer
-        } else {
-            footer.classList.remove('show'); // Remove 'show' class to hide it
-        }
+    footer.classList.add('show');
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        footer.classList.add('show'); // Add 'show' class to reveal the footer
     } else {
-         footer.classList.add('show');
+        footer.classList.remove('show'); // Remove 'show' class to hide it
     }
+    
 });
-public class ScrollCheck {
-    public static void main(String[] args) {
-        // Assume 'driver' is your initialized WebDriver instance
-        WebDriver driver = null; // Initialize your WebDriver here
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        boolean hasVerticalScrollbar = (boolean) js.executeScript("return document.documentElement.scrollHeight > document.documentElement.clientHeight;");
-
-        if (hasVerticalScrollbar) {
-            System.out.println("The page has a vertical scrollbar.");
-        } else {
-            footer.classList.add('show');
-        }
-        // ... (close WebDriver)
-    }
-}
