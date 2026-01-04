bb6d61207872648f5d8b0e511ea58e920d9b8d20
Surcury-2 <ijorambmak@gmail.com>
2026-01-04 15:24:09 +0000

    Added 3D Modeling to downloads tab

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ window.addEventListener('scroll', function() {
     const footer = document.querySelector('.footer');
 
     // Check if the user has scrolled to the very bottom of the page
-    if (footer) {
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-            footer.classList.add('show'); // Add 'show' class to reveal the footer
-        } else {
-            footer.classList.remove('show'); // Remove 'show' class to hide it
-        }
+    footer.classList.add('show');
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        footer.classList.add('show'); // Add 'show' class to reveal the footer
+    } else {
+        footer.classList.remove('show'); // Remove 'show' class to hide it
     }
+    
 });
+const loader = new THREE.GLTFLoader();
+
+loader.load(
+  '660cylinderReduced.gltf',
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error('Error loading model:', error);
+  }
+);
 
